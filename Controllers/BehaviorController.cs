63b4db8658db38e2b044dd5ab161ead1744a6dfe
Kristen Howton <kristen.howton@gmail.com>
2020-07-28 11:10:25 -0500

    set up behavior repo and controller, but need to set up a learner to test some functionality

---
+++ Controllers/BehaviorController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BehaviorReport.Data;
+using BehaviorReport.Models;
+using BehaviorReport.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BehaviorReport.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BehaviorController : ControllerBase
+    {
+        private readonly BehaviorRepository _behaviorRepository;
+
+        private readonly UserProfileRepository _userProfileRepository;
+
+
+        //using context instead of config
+        public BehaviorController(ApplicationDbContext context)
+        {
+            _behaviorRepository = new BehaviorRepository(context);
+            _userProfileRepository = new UserProfileRepository(context);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetActivity(int id)
+        {
+            return Ok(_behaviorRepository.GetBehaviorById(id));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_behaviorRepository.GetAllBehaviors());
+        }
+
+        [HttpGet("byuser")]
+        public IActionResult GetByUserProfile()
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            return Ok(_behaviorRepository.GetAllBehaviorsByLearner(currentUserProfile.Id));
+        }
+
+        [HttpPost]
+        public IActionResult Behavior(Behavior behavior)
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            behavior.LearnerId = currentUserProfile.Id;
+            _behaviorRepository.Add(behavior);
+            return CreatedAtAction(nameof(Get), new { id = behavior.Id }, behavior);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Behavior behavior)
+        {
+            if (id != behavior.Id)
+            {
+                return BadRequest();
+            }
+
+            _behaviorRepository.Update(behavior);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _behaviorRepository.Delete(id);
+            return NoContent();
+        }
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
---
+++ Repositories/BehaviorRepository.cs
@@ -0,0 +1,64 @@
+﻿using BehaviorReport.Data;
+using BehaviorReport.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BehaviorReport.Repositories
+{
+    public class BehaviorRepository
+    {
+        private readonly Data.ApplicationDbContext _context;
+        public BehaviorRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Behavior GetBehaviorById(int id)
+        {
+            return _context.Behavior
+                            .Where(b => !b.IsDeleted)
+                            .FirstOrDefault(b => b.Id == id);
+        }
+
+        public List<Behavior> GetAllBehaviors()
+        {
+            return _context.Behavior
+                            .Where(b => !b.IsDeleted)
+                            .Include(b => b.Learner)
+                            .ToList();
+        }
+
+        public List<Behavior> GetAllBehaviorsByLearner(int id)
+        {
+            return _context.Behavior
+                            .Include(b => b.Learner)
+                            .Where(b => !b.IsDeleted)
+                            .Where(b => b.LearnerId == id)
+                            .ToList();
+        }
+
+        public void Add(Behavior behavior)
+        {
+            _context.Add(behavior);
+            _context.SaveChanges();
+        }
+
+        public void Update(Behavior behavior)
+        {
+            _context.Entry(behavior).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var behavior = GetBehaviorById(id);
+            behavior.IsDeleted = true;
+            _context.Entry(behavior).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+    }
+}
