75b67fc56c1c3c6d4121de5a9996da44c58ba899
Kristen Howton <kristen.howton@gmail.com>
2020-07-30 14:17:29 -0500

    report repo and controller set up and they work in postman

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BehaviorReport.Data;
+using BehaviorReport.Models;
+using BehaviorReport.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BehaviorReport.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly ReportRepository _reportRepository;
+        private readonly UserProfileRepository _userProfileRepository;
+
+
+        //using context instead of config
+        public ReportController(ApplicationDbContext context)
+        {
+            _reportRepository = new ReportRepository(context);
+
+            _userProfileRepository = new UserProfileRepository(context);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetReport(int id)
+        {
+            return Ok(_reportRepository.GetReportById(id));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_reportRepository.GetAllReports());
+        }
+
+        [HttpGet("bylearner")]
+        public IActionResult GetByLearner()
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            return Ok(_reportRepository.GetReportByLearner(currentUserProfile.Id));
+        }
+
+        [HttpPost]
+        public IActionResult Report(Report report)
+        {
+            var currentUserProfile = GetCurrentUserProfile();
+            report.LearnerId = currentUserProfile.Id;
+            _reportRepository.Add(report);
+            return CreatedAtAction(nameof(Get), new { id = report.Id }, report);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Report report)
+        {
+            if (id != report.Id)
+            {
+                return BadRequest();
+            }
+
+            _reportRepository.Update(report);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _reportRepository.Delete(id);
+            return NoContent();
+        }
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
